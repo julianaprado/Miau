c0b056c9ed65bd9d13e69aef750c2dfaa9581dde
julianahprado <julianaprado@me.com>
2021-12-05 13:31:39 -0300

    first

---
+++ Miau/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Miau
+//
+//  Created by Juliana Prado on 05/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Miau/MiauApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiauApp.swift
+//  Miau
+//
+//  Created by Juliana Prado on 05/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MiauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
